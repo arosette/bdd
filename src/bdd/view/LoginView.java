cd9e95e3bd4bcc0a988818663f3925712aa7ee0d
nicoomer <nicoomer@ulb.ac.be>
2014-05-01 23:04:21 +0200

    ajout d'une fonction de vérification du format de l'adresse e-mail

--- src/bdd/view/LoginView.java
+++ src/bdd/view/LoginView.java
@@ -1,5 +1,6 @@
 package bdd.view;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
@@ -11,41 +12,63 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class LoginView extends JDialog {
+
+    private JPanel mainPanel;
+    private JTextField emailTextField;
+    private JTextField passwordTextField;
+    
+    public LoginView() {
+	this.setTitle("Fenêtre d'authentification");
+	this.setSize(300, 200);
+	this.build();
+	this.setContentPane(this.mainPanel);
+	this.setVisible(true);
+    }
+
+    public void build() {
+	this.mainPanel = new JPanel();
+	this.mainPanel.setLayout(new GridBagLayout());
+
+	GridBagConstraints gbc = new GridBagConstraints();
+
+	gbc.gridx = 0;
+	gbc.gridy = 0;
+
+	gbc.gridheight = 1;
+	gbc.gridwidth = 1;
+
+	emailTextField = new JTextField(10);
+	mainPanel.add(new JLabel("Adresse mail : "), gbc);
+	gbc.gridx = 1;
+	mainPanel.add(emailTextField, gbc);
+	gbc.gridx = 0;
+	gbc.gridy = 1;
 	
-	private JPanel mainPanel;
-	
-	public LoginView() {
-		this.setTitle("FenÃªtre d'authentification");
-		this.setSize(300, 200);
-		this.build();
-		this.setContentPane(this.mainPanel);
-		this.setVisible(true);
+	passwordTextField = new JPasswordField(10);
+	mainPanel.add(new JLabel("Mot de passe : "), gbc);
+	gbc.gridx = 1;
+	mainPanel.add(passwordTextField, gbc);
+
+	gbc.gridx = 0;
+	gbc.gridy = 2;
+	gbc.gridwidth = 2;
+	mainPanel.add(new JButton("Connexion"), gbc);
+    }
+
+    /**
+     * Method to check if email text field is in correct email format
+     * 
+     * @return true if email field is in "someone@example.com" format
+     */
+    public boolean checkIfEmailFieldOk() {
+	String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+	if (emailTextField.getText().matches(EMAIL_REGEX)) {
+	    return true;
 	}
-	
-	public void build() {
-		this.mainPanel = new JPanel();
-		this.mainPanel.setLayout(new GridBagLayout());
-		
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		
-		gbc.gridheight = 1;
-		gbc.gridwidth = 1;
-		
-		mainPanel.add(new JLabel("Adresse mail : "), gbc);
-		gbc.gridx = 1;
-		mainPanel.add(new JTextField(10), gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		mainPanel.add(new JLabel("Mot de passe : "), gbc);
-		gbc.gridx = 1;
-		mainPanel.add(new JPasswordField(10), gbc);
-		
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.gridwidth = 2;
-		mainPanel.add(new JButton("Connexion"), gbc);
+
+	else {
+	    emailTextField.setBackground(new Color(200, 0, 0));
+	    return false;
 	}
+    }
 }
