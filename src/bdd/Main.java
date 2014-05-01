0932c9a1dd9f5b2d7916736271be2fe578c79321
nicoomer <nicoomer@ulb.ac.be>
2014-05-01 22:39:24 +0200

    little test

--- src/bdd/Main.java
+++ src/bdd/Main.java
@@ -5,7 +5,7 @@ import bdd.view.LoginView;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Hello World from Nicolas !");
 		new LoginView();
 	}
 
