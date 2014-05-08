278d91e91cc00ca0aebcbfce370ca8cbeff41bbc
Arnaud Rosette <arnaud.rosette@outlook.com>
2014-05-08 13:56:24 +0200

    Modification de la fenetre de publication : creation avec window builder.

--- src/bdd/view/PublicationView.java
+++ src/bdd/view/PublicationView.java
@@ -1,99 +1,96 @@
 package bdd.view;
 
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.EventQueue;
 
-import javax.swing.JCheckBox;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
+import javax.swing.JCheckBox;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import java.awt.Insets;
 import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JTextArea;
 
 public class PublicationView extends JDialog {
+	private JTextField associatedStreamTextfield;
 
-    private JPanel mainPanel;
-    private JCheckBox publicationRed;
-    private JTextField associatedStreamName;
-    private JTextArea publicationDescription;
-    private JTextField publicationUrl;
-    private JTextField publicationDate;
-    
-    public PublicationView(JFrame parent) {
-	super(parent);
-	setTitle("Publication");
-	setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-	setSize(800, 600);
-	build();
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+	EventQueue.invokeLater(new Runnable() {
+	    public void run() {
+		try {
+		    PublicationView dialog = new PublicationView();
+		    dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		    dialog.setVisible(true);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	    }
+	});
     }
-    
-    private void build() {
-	int defaultColumnLength = 20;
-	int defaultRowLength = 10;
+
+    /**
+     * Create the dialog.
+     */
+    public PublicationView() {
+	setBounds(100, 100, 451, 480);
+	GridBagLayout gridBagLayout = new GridBagLayout();
+	gridBagLayout.columnWidths = new int[]{0, 0, 0};
+	gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
+	gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+	gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+	getContentPane().setLayout(gridBagLayout);
 	
+	JCheckBox publicationReadCheckBox = new JCheckBox("Lue");
+	publicationReadCheckBox.setForeground(Color.BLACK);
+	publicationReadCheckBox.setEnabled(false);
+	GridBagConstraints gbc_publicationReadCheckBox = new GridBagConstraints();
+	gbc_publicationReadCheckBox.anchor = GridBagConstraints.WEST;
+	gbc_publicationReadCheckBox.gridwidth = 2;
+	gbc_publicationReadCheckBox.insets = new Insets(0, 0, 5, 0);
+	gbc_publicationReadCheckBox.gridx = 0;
+	gbc_publicationReadCheckBox.gridy = 0;
+	getContentPane().add(publicationReadCheckBox, gbc_publicationReadCheckBox);
 	
-	this.mainPanel = new JPanel();
-	this.mainPanel.setLayout(new GridBagLayout());
+	JLabel streamLabel = new JLabel("Flux : ");
+	GridBagConstraints gbc_streamLabel = new GridBagConstraints();
+	gbc_streamLabel.insets = new Insets(0, 0, 5, 5);
+	gbc_streamLabel.anchor = GridBagConstraints.WEST;
+	gbc_streamLabel.gridx = 0;
+	gbc_streamLabel.gridy = 1;
+	getContentPane().add(streamLabel, gbc_streamLabel);
 	
-	GridBagConstraints gbc = new GridBagConstraints();
-	gbc.gridx = 0;
-	gbc.gridy = 0;
-	gbc.gridheight = 1;
-	gbc.gridwidth = 2;
+	associatedStreamTextfield = new JTextField();
+	associatedStreamTextfield.setEditable(false);
+	GridBagConstraints gbc_associatedStreamTextfield = new GridBagConstraints();
+	gbc_associatedStreamTextfield.insets = new Insets(0, 0, 5, 0);
+	gbc_associatedStreamTextfield.fill = GridBagConstraints.HORIZONTAL;
+	gbc_associatedStreamTextfield.gridx = 1;
+	gbc_associatedStreamTextfield.gridy = 1;
+	getContentPane().add(associatedStreamTextfield, gbc_associatedStreamTextfield);
+	associatedStreamTextfield.setColumns(10);
 	
-	publicationRed = new JCheckBox("Lue");
-	publicationRed.setOpaque(false);
-	publicationRed.setEnabled(false);
-	this.mainPanel.add(publicationRed, gbc);
+	JLabel descriptionLabel = new JLabel("Description : ");
+	GridBagConstraints gbc_descriptionLabel = new GridBagConstraints();
+	gbc_descriptionLabel.insets = new Insets(0, 0, 5, 0);
+	gbc_descriptionLabel.gridwidth = 2;
+	gbc_descriptionLabel.gridx = 0;
+	gbc_descriptionLabel.gridy = 2;
+	getContentPane().add(descriptionLabel, gbc_descriptionLabel);
 	
-	gbc.gridy = 1;
-	gbc.gridwidth = 1;
-	this.mainPanel.add(new JLabel("Flux : "), gbc);
-	
-	gbc.gridx = 1;
-	associatedStreamName = new JTextField(defaultColumnLength);
-	associatedStreamName.setEditable(false);
-	associatedStreamName.setBackground(Color.white);
-	this.mainPanel.add(associatedStreamName, gbc);
-	
-	gbc.gridx = 0;
-	gbc.gridy = 2;
-	gbc.gridwidth = 2;
-	this.mainPanel.add(new JLabel("Description : "), gbc);
-	
-	gbc.gridy = 3;
-	publicationDescription = new JTextArea(defaultRowLength, defaultColumnLength);
-	publicationDescription.setEditable(false);
-	publicationDescription.setBackground(Color.white);
-	this.mainPanel.add(publicationDescription, gbc);
-	
-	gbc.gridwidth = 1;
-	gbc.gridx = 0;
-	gbc.gridy = 4;
-	this.mainPanel.add(new JLabel("Url : "), gbc);
-	
-	gbc.gridx = 1;
-	publicationUrl = new JTextField(defaultColumnLength);
-	publicationUrl.setEditable(false);
-	publicationUrl.setBackground(Color.white);
-	this.mainPanel.add(publicationUrl, gbc);
-	
-	gbc.gridx = 0;
-	gbc.gridy = 5;
-	this.mainPanel.add(new JLabel("Date : "), gbc);
-	
-	gbc.gridx = 1;
-	publicationDate = new JTextField(defaultColumnLength);
-	publicationDate.setEditable(false);
-	publicationDate.setBackground(Color.white);
-	this.mainPanel.add(publicationDate, gbc);
-	
-	
-	
-	
-	this.setContentPane(this.mainPanel);
-	this.setVisible(true);
+	JTextArea textArea = new JTextArea();
+	textArea.setColumns(10);
+	textArea.setRows(5);
+	GridBagConstraints gbc_textArea = new GridBagConstraints();
+	gbc_textArea.gridwidth = 2;
+	gbc_textArea.fill = GridBagConstraints.BOTH;
+	gbc_textArea.gridx = 0;
+	gbc_textArea.gridy = 3;
+	getContentPane().add(textArea, gbc_textArea);
+
     }
 }
